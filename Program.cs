2fec2f184aa2ad27d0b85d24ce602cd625f1d0ee
Drew <littonandrew@gmail.com>
2019-06-21 13:34:47 -0500

    updated the dowhile loop and updated the press enter to continue

--- Program.cs
+++ Program.cs
@@ -31,110 +31,63 @@ namespace DrewsFirstProject
             Console.ReadLine();
 
             Console.WriteLine("Here's your first question:");
+            Console.WriteLine("(Choose a lower case letter)");
+            Console.WriteLine();
+            do
+            {
+                Console.WriteLine("Who won the 2019 Super Bowl?");
+                Console.WriteLine("a. Atlanta Falcons");
+                Console.WriteLine("b. Oakland Raiders");
+                Console.WriteLine("c. New England Patriots");
+                Console.WriteLine("d. Tampa Bay Bucs");
 
-            Console.WriteLine("Who won the 2019 Super Bowl?");
-            Console.WriteLine("a. Atlanta Falcons");
-            Console.WriteLine("b. Oakland Raiders");
-            Console.WriteLine("c. New England Patriots");
-            Console.WriteLine("d. Tampa Bay Bucs");
+                response = Console.ReadLine();
+                if (response != "c")
+                {
+                    Console.WriteLine("That's Incorrect!");
+                    Console.WriteLine("Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("Wow! You're smart! Give yourself a point!");
+                }
+            }
+            while (response != "c");
 
-            response = Console.ReadLine();
-            if(response != "c")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");                
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself a point!");
-            }
-            response = Console.ReadLine();
-            if (response != "c")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself a point!");
-            }
-            response = Console.ReadLine();
-            if (response != "c")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself a point!");
-            }
-            response = Console.ReadLine();
-            if (response != "c")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself a point!");
-            }
-
-            Console.WriteLine("Press Any Key to Continue");
+            Console.WriteLine();
+            Console.WriteLine("Press Enter to Continue");
             Console.ReadKey(); 
 
             Console.WriteLine("Here is your next question:");
-            Console.WriteLine("What is the name of Auburn University's tiger mascot?");
-            Console.WriteLine("a. Big Al");
-            Console.WriteLine("b. Tiger Paw");
-            Console.WriteLine("c. Mike the Tiger");
-            Console.WriteLine("d. Aubie");
+            Console.WriteLine("(Choose a lower case letter)");
+            Console.WriteLine();
 
-            response = Console.ReadLine();
-            if (response != "d")
+            do
             {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself another point!");
-            }
-            response = Console.ReadLine();
-            if (response != "d")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself another point!");
-            }
-            response = Console.ReadLine();
-            if (response != "d")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself another point!");
-            }
-            response = Console.ReadLine();
-            if (response != "d")
-            {
-                Console.WriteLine("That's Incorrect!");
-                Console.WriteLine("Try again.");
-            }
-            else
-            {
-                Console.WriteLine("Wow! You're smart! Give yourself another point!");
-            }
+                Console.WriteLine("What is the name of Auburn University's tiger mascot?");
+                Console.WriteLine("a. Big Al");
+                Console.WriteLine("b. Tiger Paw");
+                Console.WriteLine("c. Mike the Tiger");
+                Console.WriteLine("d. Aubie");
 
-            Console.WriteLine("Press Any Key to Continue");
-            response = Console.ReadLine();
+                response = Console.ReadLine();
+                if (response != "d")
+                {
+                    Console.WriteLine("That's Incorrect!");
+                    Console.WriteLine("Try again.");
+                }
+                else
+                {
+                    Console.WriteLine("DING DING DING! Give yourself a point!");
+                }
+            }
+            while (response != "d");
 
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadKey();
+            Console.WriteLine();
 
-
+            
         }
     }
 }
